e7dde75d1136efaa116c88b4bda9d583adeac27c
ilayas2002 <100944802+ilayas2002@users.noreply.github.com>
2023-04-20 14:08:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -162,7 +162,7 @@ model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['ac
 train_samples = len(X_train)
 val_samples = len(X_test)
 batch_size = 128
-epochs = 15
+epochs = 5
 model.fit_generator(generator = generate_batch(X_train, y_train, batch_size = batch_size),
                     steps_per_epoch = train_samples//batch_size,
                     epochs=epochs,
@@ -176,3 +176,64 @@ model.load_weights('nmt_weights.h5')
 # Encode the input sequence to get the "thought vectors"
 encoder_model = Model(encoder_inputs, encoder_states)
 
+# Decoder setup
+# Below tensors will hold the states of the previous time step
+decoder_state_input_h = Input(shape=(latent_dim,))
+decoder_state_input_c = Input(shape=(latent_dim,))
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+
+dec_emb2= dec_emb_layer(decoder_inputs) # Get the embeddings of the decoder sequence
+
+# To predict the next word in the sequence, set the initial states to the states from the previous time step
+decoder_outputs2, state_h2, state_c2 = decoder_lstm(dec_emb2, initial_state=decoder_states_inputs)
+decoder_states2 = [state_h2, state_c2]
+decoder_outputs2 = decoder_dense(decoder_outputs2) # A dense softmax layer to generate prob dist. over the target vocabulary
+
+# Final decoder model
+decoder_model = Model(
+    [decoder_inputs] + decoder_states_inputs,
+    [decoder_outputs2] + decoder_states2)
+
+def decode_sequence(input_seq):
+    # Encode the input as state vectors.
+    states_value = encoder_model.predict(input_seq)
+    # Generate empty target sequence of length 1.
+    target_seq = np.zeros((1,1))
+    # Populate the first character of target sequence with the start character.
+    target_seq[0, 0] = target_token_index['START_']
+
+    # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size 1).
+    stop_condition = False
+    decoded_sentence = ''
+    while not stop_condition:
+        output_tokens, h, c = decoder_model.predict([target_seq] + states_value)
+
+        # Sample a token
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_char = reverse_target_char_index[sampled_token_index]
+        decoded_sentence += ' '+sampled_char
+
+        # Exit condition: either hit max length
+        # or find stop character.
+        if (sampled_char == '_END' or
+           len(decoded_sentence) > 50):
+            stop_condition = True
+
+        # Update the target sequence (of length 1).
+        target_seq = np.zeros((1,1))
+        target_seq[0, 0] = sampled_token_index
+
+        # Update states
+        states_value = [h, c]
+
+    return decoded_sentence
+
+train_gen = generate_batch(X_train, y_train, batch_size = 1)
+k=-1
+k+=1
+(input_seq, actual_output), _ = next(train_gen)
+decoded_sentence = decode_sequence(input_seq)
+print('Input English sentence:', X_train[k:k+1].values[0])
+print('Actual Marathi Translation:', y_train[k:k+1].values[0][6:-4])
+print('Predicted Marathi Translation:', decoded_sentence[:-4])
